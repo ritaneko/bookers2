60c1eb8f6240c150c63f2a6edb62f72d6fdef888
EC2 Default User <ec2-user@ip-172-31-36-53.ap-northeast-1.compute.internal>
2023-12-28 11:00:00 +0000

    [Finish 3-27]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :introduction, :profile_image_id)
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 
   def is_matching_login_user
